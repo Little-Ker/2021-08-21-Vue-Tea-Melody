b17b63bde0a219bf26f51c910e54233f957001be
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2021-08-03 14:44:04 +0800

    [修改] 門市據點-地圖帶入資料

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,9 +4,9 @@
             <div class="chooseCity">
                 縣市
                 <select id="cityName" class="form-control" v-model="select.city">
-                    <option value="null">請選擇縣市</option>
-                    <option :value="item" v-for="item in cityList.cityList" :key="item">
-                        {{ item }}
+                    <!-- <option value="null">請選擇縣市</option> -->
+                    <option :value="item.city" v-for="item in cityList" :key="item.city">
+                        {{ item.city }}
                     </option>
                 </select>
             </div>
@@ -14,12 +14,11 @@
                 地區
                 <select id="areaName" class="form-control" v-model="select.area">
                     <option value="null">請選擇地區</option>
-                    <option :value="item" v-for="item in areaList" :key="item">
-                        {{ item }}
+                    <option :value="item.area" v-for="item in areaList" :key="item.area">
+                        {{ item.area }}
                     </option>
                 </select>
             </div>
-            
         </div>
         <div id="mapBlock"></div>
     </div>
@@ -27,6 +26,7 @@
 
 <script>
 import shopPointData from '../assets/data/shopPointData.json';
+import cityCenter from '../assets/data/cityCenter.json';
 
 import jQuery from "jquery";
 const $ = jQuery;
@@ -42,6 +42,7 @@ export default {
     data() {
         return {
             shopPointData,
+            cityCenter,
             select: {
                 city: '台中市',
                 area: 'null'
@@ -50,113 +51,109 @@ export default {
     },
     computed: {
         shopList() {
-            return this.shopPointData.filter((pharmacy) => {
-                if (!this.select.area) {
-                    return pharmacy.properties.county === this.select.city;
+            return this.shopPointData.shop.filter((shop) => {
+                if (this.select.area === 'null') {
+                    return shop.city === this.select.city;
                 }
-                return pharmacy.properties.town === this.select.area;
+                return shop.area === this.select.area;
             });
         },
-
+        setMapCenter() {
+            return this.cityCenter.center.filter((city) => {
+                return city.city === this.select.city;
+            });
+        },
         cityList() {
-            let obj = {
-                cityList: [],
-                cityObj: {}
-            }
-
-            this.shopPointData.shop.forEach(({
-                city,
-                area,
-            }) => {
-                if (!obj.cityObj[city]) {
-                    obj.cityList.push(city);
-                    obj.cityObj[city] = {
-                        areaObj: {},
-                        areaList: [],
-                    }
-                }
-                if (!obj.cityObj[city].areaObj[area]) {
-                    obj.cityObj[city].areaList.push(area);
-                    obj.cityObj[city].areaObj[area] = {}
-                }
-            })
-            console.log('obj',obj);
-            return obj;
+            return this.cityCenter.center.filter((city) => {
+                return city;
+            });
         },
         areaList() {
             this.clearSelectArea();
             if (this.select.city !== 'null') {
-                return this.cityList.cityObj[this.select.city].areaList
+                return this.setMapCenter[0].areaList
             }
             return [];
         },
+        chooseArea() {
+            return this.setMapCenter[0].areaList.filter((area) => {
+                return area.area === this.select.area;
+            });
+        }
     },
     methods: {
        clearSelectArea() {
            this.select.area = 'null';
        },
+       setCityCenter() {
+            if (this.select.area === 'null') {
+                const geometry = this.setMapCenter[0].geometry;
+                openStreetMap.panTo(geometry)
+                if (openStreetMap._zoom === 10) return; 
+                this.timeout = setTimeout(() => {
+                    openStreetMap.setZoom(10);
+                }, 300);
+                return;
+            }
+            const geometry = this.chooseArea[0].geometry;
+            openStreetMap.panTo(geometry)
+            if (openStreetMap._zoom === 13) return; 
+            this.timeout = setTimeout(() => {
+                openStreetMap.setZoom(13);
+            }, 300);
+       },
        updateMap() {
-            // clear markers
+            // 清除所有標記
             openStreetMap.eachLayer((layer) => {
                 if (layer instanceof L.Marker) {
-                openStreetMap.removeLayer(layer);
+                    openStreetMap.removeLayer(layer);
                 }
             });
 
             // 客製化icon
             const customIcon = L.icon({
-                iconUrl: 'https://img-premium.flaticon.com/png/512/1078/1078454.png?token=exp=1627801949~hmac=5f95a748b7120a098db1d4473f0defab',
+                iconUrl: require('../assets/mapPoint.png'),
                 iconSize: [42, 42],
             });
         
             // add markers
-            this.pharmacies.forEach((pharmacy) => {
-                const center = [pharmacy.geometry.coordinates[1], pharmacy.geometry.coordinates[0]];
+            this.shopList.forEach((shop) => {
+                const point = [shop.geometry.x, shop.geometry.y];
                 // 透過藥局經緯度疊加標記
-                const marker = L.marker(center, {
+                const marker = L.marker(point, {
                     icon: customIcon,
                     title: '跟 <a> 的 title 一樣', // 跟 <a> 的 title 一樣
                     opacity: 1.0,
                 }
                 ).addTo(openStreetMap);
 
-                // 地圖中心點改變
-                openStreetMap.panTo(center)
-
-                // 圖形標記
-                // L.circle(center, {
-                //     color: 'red',
-                //     fillColor: '#f03',
-                //     fillOpacity: 0.5,
-                //     radius: 500
-                // }).addTo(openStreetMap);
-
                 // 點選標記時有說明框
-                marker.bindPopup(`<p><strong style="font-size: 20px;">${pharmacy.properties.name}</strong></p>
-                    <strong style="font-size: 16px; color: #d45345;">口罩剩餘：成人 - ${pharmacy.properties.mask_adult ? `${pharmacy.properties.mask_adult} 個` : '未取得資料'} / 兒童 - ${pharmacy.properties.mask_child ? `${pharmacy.properties.mask_child} 個` : '未取得資料'}</strong><br>
-                    地址: ${pharmacy.properties.address}<br>
-                    電話: ${pharmacy.properties.phone}<br>
-                    <small>最後更新時間: ${pharmacy.properties.updated}</small>`);
+                marker.bindPopup(`<p><strong style="font-size: 20px;">${shop.shopName}</strong></p>
+                    <p style="font-size: 15px;">地址: ${shop.address}<br>電話: ${shop.phone}<br></p>`);
 
                 // 滑鼠靠近地標時有說明文字
-                marker.bindTooltip("my tooltip text", {
-                    direction: 'bottom', // right、left、top、bottom、center。default: auto
-                    sticky: false, // true 跟著滑鼠移動。default: false
-                    permanent: false, // 是滑鼠移過才出現，還是一直出現
+                marker.bindTooltip(`${shop.shopName}`, {
+                    direction: 'right', // right、left、top、bottom、center
+                    sticky: false, // 跟著滑鼠移動
+                    permanent: true, // 是滑鼠移過才出現，還是一直出現
                     opacity: 1.0
                 }).openTooltip();
-
             })
-        }
+
+            // 地圖中心點改變
+            this.setCityCenter();
+        },
     },
     watch: {
+        shopList() {
+            this.updateMap();
+        },
     },
     mounted() {
         const centerPoint = [24.9983469, 121.5810358];
         // 生成地圖
         openStreetMap = L.map('mapBlock', {
         center: centerPoint,
-        // 可以嘗試改變 zoom 的數值
         zoom: 19,
         zoomControl: true , // 是否秀出 - + 按鈕
         });
@@ -164,6 +161,18 @@ export default {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 19,
         }).addTo(openStreetMap);
+
+        this.updateMap();
+
+        // // 點擊地圖取得經緯度
+        // var popup = L.popup();
+        // function onMapClick(e) {
+        //     popup
+        //         .setLatLng(e.latlng)
+        //         .setContent("經緯度座標：" + e.latlng.toString())
+        //         .openOn(openStreetMap);
+        // }
+        // openStreetMap.on('click', onMapClick);
     },
 }
 </script>
@@ -178,7 +187,6 @@ export default {
 #mapBlock {
     max-width: 1280px;
     z-index: 1;
-    /* margin: 10px; */
     height: 560px;
     width: 90%;
     margin: auto;
@@ -188,8 +196,13 @@ export default {
     padding-bottom: 40px;
 }
 
+.choose {
+    margin-bottom: 30px;
+}
+
 @media (max-width: 460px) {
     #mapBlock {
+        height: 500px;
         width: 90%;
     }
 }
